c696722a0d4b8727e51b9ed2ef1fbccffa4d8383
marko1olo <96879250+marko1olo@users.noreply.github.com>
2026-06-22 10:59:33 +0000

    🔒 [security] Fix hardcoded server root password

--- deploy_vps.py
+++ deploy_vps.py
@@ -1,9 +1,13 @@
 import paramiko
 import sys
+import os
 
 host = '62.84.100.97'
 user = 'root'
-password = 'W15n8zf781%nV25BGZ+2'
+password = os.environ.get('SERVER_PASSWORD')
+if not password:
+    print('SERVER_PASSWORD environment variable not set.', file=sys.stderr)
+    sys.exit(1)
 
 def ssh(client, cmd, desc="", timeout=90):
     label = desc or cmd[:60]

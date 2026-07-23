3d3a3cb83b8beea1b3b61d26e64cce372a1c75e5
marko1olo <96879250+marko1olo@users.noreply.github.com>
2026-07-23 09:34:31 +0000

    Add unit test for check_tcp_socks5

---
+++ reply.py
@@ -0,0 +1,10 @@
+from default_api import reply_to_pr_comments
+
+reply_to_pr_comments(
+    replies='''[
+        {
+            "comment_id": "5056799928",
+            "reply": "Understood. Acknowledging that this work is now obsolete and stopping work on this task."
+        }
+    ]'''
+)
